3b489200cc4f213023f41d7f904c863d8b1cd4cf
Admazzola <admazzola@gmail.com>
2023-01-18 15:39:29 -0500

    so close but invalid sig

--- tasks/executeReservoirOrder.ts
+++ tasks/executeReservoirOrder.ts
@@ -4,6 +4,7 @@
 
 
 
+import { recoverSignerOfOffchainOffer } from '@clarity-credit/anpl-sdk'
 import axios from 'axios'
 import { toChecksumAddress } from 'ethereumjs-util'
 import {Contract, Wallet, providers, utils, BigNumber, ethers} from 'ethers'
@@ -115,7 +116,7 @@ export async function executeReservoirOrder(): Promise<any> {
         signature: basicOrderParams.signature
     }
  
-
+    console.log({rpcURI})
     console.log({bnplConfig})
 
     let rpcProvider = new providers.JsonRpcProvider( rpcURI )
@@ -131,7 +132,7 @@ export async function executeReservoirOrder(): Promise<any> {
     const borrowerAddress = borrowerWallet.address
 
     
-    const lenderWallet = new Wallet(lenderPrivateKey)
+    const lenderWallet = new Wallet(lenderPrivateKey,rpcProvider)
 
     const lenderAddress = lenderWallet.address
 
@@ -181,20 +182,7 @@ export async function executeReservoirOrder(): Promise<any> {
         verifyingContract: implementationContractAddress
     }
 
-    let borrowerSignature = await generateBNPLOrderSignature( 
-        submitBidArgs,
-        basicOrderParamsFormatted,  
-        domainData, 
-        borrowerWallet,      
-       ) 
-
-    let lenderSignature = await generateBNPLOrderSignature( 
-        submitBidArgs,
-        basicOrderParamsFormatted,   
-        domainData,
-        lenderWallet,       
-        ) 
-
+  
 
         //fix it for now to remove referral and sig expir
       let formattedSubmitBidArgs:SubmitBidArgs = {
@@ -213,6 +201,30 @@ export async function executeReservoirOrder(): Promise<any> {
 
 
 
+      let borrowerSignature = await generateBNPLOrderSignature( 
+        formattedSubmitBidArgs,
+        basicOrderParamsFormatted,  
+        domainData, 
+        borrowerWallet,      
+       ) 
+
+    let lenderSignature = await generateBNPLOrderSignature( 
+        formattedSubmitBidArgs,
+        basicOrderParamsFormatted,   
+        domainData,
+        lenderWallet,       
+        ) 
+
+
+    let recoveredSigner = recoverSignerOfOffchainOffer(
+        formattedSubmitBidArgs,
+        basicOrderParamsFormatted,   
+        domainData,
+        lenderSignature
+    )
+
+    console.log({recoveredSigner})
+
     console.log(JSON.stringify(basicOrderParamsFormatted))
     console.log(JSON.stringify(formattedSubmitBidArgs))
 
@@ -224,7 +236,7 @@ export async function executeReservoirOrder(): Promise<any> {
 
 
  
-    console.log({borrowerSignature})
+    console.log({borrowerSignature},{lenderSignature})
 
     let unsignedTx = await bnplContractInstance
     .populateTransaction
@@ -241,188 +253,7 @@ export async function executeReservoirOrder(): Promise<any> {
     let response = await borrowerWallet.sendTransaction(unsignedTx);
     console.log('response',response)
 
-
-
-  /*
-
-    let rpcProvider = new providers.JsonRpcProvider( rpcURI )
-    
-    let tellerV2Instance = new Contract(tellerV2Config.address,tellerV2Config.abi, rpcProvider)
-    let bnplContractInstance = new Contract(bnplConfig.address,bnplConfig.abi,rpcProvider)
-
-
-    let signatureVersion = await readSignatureVersionFromBNPLMarketContract(  bnplContractInstance )
-
-    let craInputs = {
-      asset_contract_address:tokenInputData.tokenAddress,
-      token_id:tokenInputData.tokenId,
-      quantity: tokenInputData.tokenQuantity,
  
-      chain_id:tokenInputData.chainId,    
-      signature_version: signatureVersion
-    }
-
-   // let craResponse = await performCraRequest( craInputs  )
-    let craResponse = {success:true, data: craResponseSample , error:'none'}
-
-    console.log('meep', craResponse)
-    
-
-    if(!craResponse.success || !craResponse.data) throw new Error('cra error '.concat(craResponse.error.toString()))
-  
-
-    let executeParams = craResponse.data 
-
-    if(typeof(executeParams.submitBidArgs.metadataURI) == 'undefined'){
-      executeParams.submitBidArgs.metadataURI = "ipfs://"
-    }
- 
-
-    if(!borrowerPrivateKey) throw new Error('Missing borrowerPrivateKey')
-
-
-    if(!lenderPrivateKey) throw new Error('Missing lenderPrivateKey')
-
-    let borrowerWallet = new Wallet(borrowerPrivateKey).connect(rpcProvider)
-
-    let lenderWallet = new Wallet(lenderPrivateKey).connect(rpcProvider)
- 
-    console.log(`calling execute using account ${borrowerWallet.address}`)
-  
- 
-    const submitBidArgs:SubmitBidArgs = executeParams.submitBidArgs
-
-
-    submitBidArgs.borrower = borrowerWallet.address
-    submitBidArgs.lender = borrowerWallet.address
-
-    let value:BigNumber = BigNumber.from(submitBidArgs.downPayment)      
-
-    let lenderAddress = lenderWallet.address
-
-    let basicOrderParams:BasicOrderParams = executeParams.basicOrderParams
-
-    if(!basicOrderParams.offererConduitKey){
-      throw new Error('Missing offererConduitKey')
-    }
-
-    */
-
-
-    /*
-    const chainId = tokenInputData.chainId
-
-
-
-    // find implementation address via proxy admin 
-
-    let bnplContractProxyAddress = contractsConfig.BNPLContract.address
-
-    const proxyAdminContract = new Contract( contractsConfig.proxyAdmin.address, ProxyAdminInterface, rpcProvider )
-    let implementationContractAddress = await proxyAdminContract.getProxyImplementation( bnplContractProxyAddress )
-
-    implementationContractAddress = toChecksumAddress(implementationContractAddress)
-
-    if(typeof(implementationContractAddress) == 'undefined'){
-      return {success:false, error:"Could not get implementation address"}
-    }
- 
-
-
-      let lenderSignature = await generateBNPLOrderSignature( 
-        submitBidArgs,
-        basicOrderParams,   
-        lenderWallet,
-        chainId,
-        implementationContractAddress
-       ) 
-
-       let borrowerSignature = await generateBNPLOrderSignature( 
-        submitBidArgs,
-        basicOrderParams,   
-        borrowerWallet,
-        chainId,
-        implementationContractAddress
-       ) 
-
-
-
- 
-    let lenderHasApproved = await tellerV2Instance.hasApprovedMarketForwarder(executeConfig.marketplaceId, bnplContractInstance.address, lenderAddress)
-    console.log('lender has approved BNPL as forwarder: ',lenderHasApproved, lenderAddress)
-
-    if(!lenderHasApproved) {
-        console.error(`ERROR: lender ${lenderAddress} has not approved bnpl as forwarder `)
-        return 
-    }
-
-
-    let borrowerHasApproved = await tellerV2Instance.hasApprovedMarketForwarder(executeConfig.marketplaceId, bnplContractInstance.address, lenderAddress)
-    console.log('lender has approved BNPL as forwarder: ',borrowerHasApproved, lenderAddress)
-
-    if(!borrowerHasApproved) {
-        console.error(`ERROR: lender ${lenderAddress} has not approved bnpl as forwarder `)
-        return 
-    }
-
-
-
-
-      //fix it for now to remove referral and sig expir
-      let formattedSubmitBidArgs:SubmitBidArgs = {
-        lender: lenderAddress,
-        totalPurchasePrice: submitBidArgs.totalPurchasePrice,
-        principal: submitBidArgs.principal,
-        downPayment: submitBidArgs.downPayment,       
-        duration: submitBidArgs.duration,       
-        signatureExpiration: submitBidArgs.signatureExpiration,
-        interestRate:submitBidArgs.interestRate,
-        referralAddress: submitBidArgs.referralAddress,
-        metadataURI: submitBidArgs.metadataURI ,
-      }
-
-
-    console.log('passing in params',
-      formattedSubmitBidArgs, 
-      basicOrderParams ,
-      borrowerSignature,
-      lenderSignature
-    )
- 
-   // basicOrderParams.offererConduitKey = "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000"
-  //basicOrderParams.signature= "0x5c8a6fd29db37f9b53fcf1cce21c62e68ca0684c28e0320cb27e8568210baf37644e1258430b698cfe533334621ddd82a12e46dd184823ab318b8ee6ff0fa2dd1c"
-
-    //this address needs to approve the forwarder on tellerv2
-  //  lenderAddress =  "0xF4dAb24C52b51cB69Ab62cDE672D3c9Df0B39681"
-
-    //Set price to 1 Gwei
-    let gasPrice = utils.hexlify(8_000_000_000);
-    //Set max gas limit to 4M
-    var gasLimit = utils.hexlify(10_000_000);  
-
-
-    if((basicOrderParams.basicOrderType) > 22){
-      throw new Error('invalid basic order type')
-    }
- 
-
-     */
-
-   /* let unsignedTx = await bnplContractInstance
-    .populateTransaction
-    .executeWithOffchainSignatures(
-      formattedSubmitBidArgs, 
-      basicOrderParams, 
-      borrowerSignature,
-      lenderSignature
-      
-      , {value, gasLimit, gasPrice} )
-
-  
-
-    let response = await borrowerWallet.sendTransaction(unsignedTx);
-    console.log('response',response)*/
-     
    
     return true 
   }
